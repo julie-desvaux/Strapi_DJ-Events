0779652896e1959f372907cd0aafd39c9a39eb61
julie-desvaux <desvaux.julie@gmail.com>
2021-08-09 20:20:39 +0200

    Strapi: Cloudinary For Images / Event Content Type

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6b8e770fa546962fcdef1c9ba46ce0dd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b8bd2f20-bc4e-4efb-842e-f88f6fc3ea86'
+};
